888b30cdeed339088f633946c9e40f3a311a8a4b
Akshat <akshatgupta099@gmail.com>
2026-05-31 16:47:05 +0530

    Add initial project setup with environment configuration, CORS support, and MongoDB connection
    - Created .env.example for environment variables
    - Updated package.json and package-lock.json to include cors and dotenv dependencies
    - Implemented CORS configuration in app.js
    - Established MongoDB connection in index.js
    - Added ApiError and ApiResponse utility classes for error handling
    - Introduced asyncHandler utility for handling asynchronous requests
    - Removed unused readme.md file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello, VidTube!");
+import dotenv from 'dotenv';
+import {app} from './app.js';
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path: "./.env"
+});
+
+const port = process.env.PORT || 8001;
+
+connectDB().then(() => {
+    app.listen(port,()=>{
+        console.log(`Server is running on port ${port}`);
+})
+}).catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+});
+
